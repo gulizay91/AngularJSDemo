598f7fbfe423d5dce6dc94df98c425935ab26376
gulizay91 <gulizay91@gmail.com>
2018-12-07 17:42:49 +0300

    userlist component refresh, not complete

--- app/components/user/list/user-list.component.js
+++ app/components/user/list/user-list.component.js
@@ -1,6 +1,6 @@
 function getData() {
   let responseData = [];
-  //responseData = fetch('/article/promise-chaining/user.json');
+  
   responseData = [
     {id: "1", username: 'sadmin', firstname:"guliz", lastname: 'ay'}
     ,{id: "2", username: 'admin', firstname:"admin", lastname: 'admin'}];
@@ -12,22 +12,28 @@ function getData() {
 async function _fetchData() {
   let datas = getData();
   let result = await datas;
-  console.log(datas)
+
+  //console.log(datas)
   return result;
 }
 
 angular.module('userApp')
   .component('userList', {
     templateUrl: 'app/components/user/list/user-list.template.html',
+    bindings: { datas: "<"},
     controller: function userListCtrl($scope, $firebaseArray) {
-      this.result = "aa";
-      $scope.$on("getList", async function(){
+      
+      $scope.$on("getList", function(){
         console.log("run child fetchData")
-        let _datas = getData();
-        this.datas = await _datas;
-        //this.result =  angular.fromJson(_datas);
-        this.result = "_datas";
-        console.log(this.result)
+        this.datas = getData();
+        $scope.$parent.datas = this.datas;
+        console.log(this.datas)
       });
+
+      this.$onChanges = function (changes) {
+        console.log("change data")
+        this.datas = [{id: "0", username: 'username', firstname:"fname", lastname: 'lname'}];
+        console.log(changes)
+      };
     }
   })
--- app/components/user/user.controller.js
+++ app/components/user/user.controller.js
@@ -1,11 +1,11 @@
 angular.module('userApp', ['firebase','ui.router'])
  .controller('userCtrl',['$scope','$firebaseArray', function($scope, $firebaseArray){
     $scope.title = "Users"
-    
+    $scope.datas = [{}];
+
     $scope.getUserList = function() {
+      //$scope.datas = [{id: "0", username: 'username2', firstname:"fname2", lastname: 'lname2'}];
       console.log("click fetchData from parent")
       $scope.$broadcast("getList"); 
-      //$scope.childApi.getList();
-      //window.location.reload();
     }
  }])
